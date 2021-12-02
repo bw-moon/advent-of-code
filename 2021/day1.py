f5a67acf7235795538b351f1fda33e2a23813817
Byeongweon Moon <bw.moon@linecorp.com>
2021-12-02 14:54:29 +0900

    Add day1 part2

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,21 +1,36 @@
-def get_deeper_count():
+def get_deeper_count(lines):
     deeper_count = 0
     previous_depth = None
-    with open('day1-input.txt') as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            depth = int(line)
-            if previous_depth is not None and depth > previous_depth:
-                deeper_count += 1
-                print(f"{depth} '(increased)'")
-            else:
-                print(f"{depth} '(decreased or same)'")
-            previous_depth = depth
+    for line in lines:
+        depth = int(line)
+        if previous_depth is not None and depth > previous_depth:
+            deeper_count += 1
+        previous_depth = depth
 
     return deeper_count
 
 
+def get_slided_lines(lines, window_size, aggregator):
+    window = []
+    result = []
+    for line in lines:
+        depth = int(line)
+
+        if len(window) >= window_size:
+            window.pop(0)
+        window.append(depth)
+
+        if len(window) < window_size:
+            continue
+
+        result.append(aggregator(window))
+    return result
+
+
 if __name__ == '__main__':
-    print(get_deeper_count())
+    with open('day1-input.txt') as f:
+        data = f.readlines()
+        print('Part1')
+        print(get_deeper_count(data))
+        print('Part2')
+        print(get_deeper_count(get_slided_lines(data, 3, sum)))
